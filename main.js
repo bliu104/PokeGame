3d5fdd35418fdfee29dd5c0e5c6f64455c405ff6
Bobby Liu <bobbyliu@bobbys-macbook.lan>
2020-02-08 01:09:21 -0500

    final

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 const pokeInput = document.querySelector("input");
-const butt = document.querySelector("button");
+const butt = document.querySelector("#button1");
 
 let pokeGet;
 
@@ -12,6 +12,36 @@ pokeGet = async function() {
   pokePic(response);
   moves(response);
 };
+
+pokeCar = async function() {
+  let i = 0;
+  let num = 1;
+  let response;
+  while (i < 13) {
+    response = await axios.get(`${BASE_URL}${num}`);
+    imageCar(response);
+    num++;
+    i++;
+  }
+  carousel();
+};
+
+function randomPoke() {
+  let randomNum = Math.floor(Math.random() * 800);
+  return randomNum;
+}
+first = async function() {
+  let i = 0;
+  let response;
+  let array = [];
+  while (i < 9) {
+    response = await axios.get(`${BASE_URL}${randomPoke()}`);
+    array[i] = response;
+    i++;
+  }
+  console.log(array);
+};
+first();
 
 function pokeData(response) {
   const pokeData = response;
@@ -31,6 +61,12 @@ function pokePic(response) {
   document.querySelector(
     ".PokePics"
   ).innerHTML = `<img src = ${imagePokeFront}>`;
+}
+
+function imageCar(response) {
+  document.querySelector(
+    ".caousel_track"
+  ).innerHTML += `<li class = 'carousel_slide'><img src = "${response.data.sprites.front_default}" class = "carousel_image"></li>`;
 }
 
 function pokeStat(response) {
@@ -62,4 +98,63 @@ function moves(response) {
 //   let imagePokeFront = pokeData.data.sprites.front_default;
 //   let typing =
 // }
+pokeCar();
+
+//------------------- carousel----------------
+
+const nextButton = document.querySelector(".carousel_button--right");
+const prevButton = document.querySelector(".carousel_button--left");
+
+function carousel() {
+  const track = document.querySelector(".caousel_track");
+  const slide = Array.from(track.children);
+  const dotsNavs = document.querySelector(".carousel_nav");
+  const dots = Array.from(dotsNavs.children);
+  const slideSize = slide[0].getBoundingClientRect().width;
+  console.log(slideSize);
+  let i = 0;
+  console.log(slide.length);
+  while (i < slide.length) {
+    slide[i].style.left = slideSize * i + "px";
+    i++;
+  }
+}
+let currentSlide;
+let nextSlide;
+let counter = 0;
+let currentPosition = 0;
+function buttonTrack() {
+  currentSlide = document.querySelector(".caousel_track").children[2 + counter];
+  nextSlide = currentSlide.nextElementSibling;
+  const amountToMove = -200;
+  document.querySelector(".caousel_track").style.transform =
+    "translateX(" + (amountToMove + currentPosition) + "px)";
+  currentSlide = nextSlide;
+  nextSlide = currentSlide.nextElementSibling;
+
+  console.log(nextSlide);
+  counter++;
+  currentPosition -= 200;
+}
+
+let rightcurrentSlide;
+let rightnextSlide;
+
+function rightButton() {
+  rightcurrentSlide = document.querySelector(".caousel_track").children[
+    2 + counter
+  ];
+  rightprevSlide = rightcurrentSlide.prevElementSibling;
+  const amountToMove = 200;
+  document.querySelector(".caousel_track").style.transform =
+    "translateX(" + (amountToMove + currentPosition) + "px)";
+  rightcurrentSlide = rightprevSlide;
+  rightprevSlide = currentSlide.prevElementSibling;
+
+  counter--;
+  currentPosition += 200;
+}
+
+nextButton.addEventListener("click", buttonTrack);
+prevButton.addEventListener("click", rightButton);
 butt.addEventListener("click", pokeGet);
